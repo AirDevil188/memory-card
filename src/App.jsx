44dc6abdf6ebe7d82f3ec6a4708cf0fa4d9c1a8b
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-02-22 17:48:02 +0100

    useEffect hook added with function expression to fetch pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
-import "./App.css";
+import { useEffect } from "react";
 
-function App() {}
+const randomNumbersArr = new Set([]);
 
-export default App;
+export default function App() {
+  useEffect(() => {
+    const key = () => {
+      console.log(randomNumbersArr, "arr");
+      randomNumbersArr.forEach(async (number) => {
+        const fetchRequest = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${number}/`
+        );
+        const data = await fetchRequest.json();
+        setCards((oldArray) => [...oldArray, data]);
+      });
+    };
+
+    return () => {
+      key();
+    };
+  }, []);
+
+  const generateRandomNumbers = (length) => {
+    const randomNumber = Math.floor(Math.random() * 150 + 1);
+    while (randomNumbersArr.size !== length) {
+      if (!randomNumbersArr.has(randomNumber))
+        randomNumbersArr.add(randomNumber);
+      else return generateRandomNumbers(length);
+    }
+  };
+
+  generateRandomNumbers(5);
+}
