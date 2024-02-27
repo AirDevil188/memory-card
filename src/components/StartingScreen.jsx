9c47b6d57d45f2a6f2f3079791feb6816840b9be
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-02-27 12:11:59 +0100

    added starting screencomponent

---
+++ src/components/StartingScreen.jsx
@@ -0,0 +1,13 @@
+export default function StartingScreen({ handleClick }) {
+  return (
+    <section className="starting-screen-section">
+      <dialog open className="dialog">
+        <div className="dialog-container">
+          <button onClick={handleClick}>Easy</button>
+          <button onClick={handleClick}>Medium</button>
+          <button onClick={handleClick}>Hard</button>
+        </div>
+      </dialog>
+    </section>
+  );
+}
