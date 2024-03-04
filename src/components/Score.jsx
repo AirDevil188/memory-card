18ba8ac7771c4a35906ef9aad5cb5669d72a34ef
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-03-04 22:55:58 +0100

    add background, mobile first approachstyle

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,6 +1,9 @@
 export default function Score({ score, highScore, length }) {
   return (
     <section className="gamescore-section">
+      <div className="title-container">
+        <h1>Poke Memo Game</h1>
+      </div>
       <div className="score-container">
         <p>Your Score: {score}</p>
         <p>Your High Score:{highScore}</p>
--- src/components/StartingScreen.jsx
+++ src/components/StartingScreen.jsx
@@ -1,14 +1,15 @@
 export default function StartingScreen({ handleClick }) {
   return (
-    <section className="starting-screen-section">
-      <h1>PokeDex Memo Game</h1>
-      <dialog open className="dialog">
-        <div className="dialog-container">
-          <button onClick={handleClick}>Easy</button>
-          <button onClick={handleClick}>Medium</button>
-          <button onClick={handleClick}>Hard</button>
-        </div>
-      </dialog>
-    </section>
+    <>
+      <section className="starting-screen-section">
+        <dialog open className="dialog">
+          <div className="dialog-container">
+            <button onClick={handleClick}>Easy</button>
+            <button onClick={handleClick}>Medium</button>
+            <button onClick={handleClick}>Hard</button>
+          </div>
+        </dialog>
+      </section>
+    </>
   );
 }
