d5b0703f70173506c6bb2f4cb0cf2c0aa5170132
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-03-05 22:16:19 +0100

    add card-content container to card element, and added border-image to it

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,12 +4,14 @@ export default function Card({ data, onClick }) {
       {data.map((card) => {
         return (
           <div className="card" key={card.id} onClick={onClick} id={card.id}>
-            <img
-              src={card.sprites.front_default}
-              alt={card.name}
-              id={card.id}
-            />
-            <h3 id={card.id}>{card.name}</h3>
+            <div className="card-content" id={card.id}>
+              <img
+                src={card.sprites.front_default}
+                alt={card.name}
+                id={card.id}
+              />
+              <h3 id={card.id}>{card.name}</h3>
+            </div>
           </div>
         );
       })}
