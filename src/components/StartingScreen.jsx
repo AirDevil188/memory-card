7fab5e6ef5760dbb60f3f7f3d9783be9cff003d3
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-03-05 23:12:54 +0100

    removed h1 from starting selection screen

--- src/components/StartingScreen.jsx
+++ src/components/StartingScreen.jsx
@@ -1,7 +1,6 @@
 export default function StartingScreen({ handleClick }) {
   return (
     <section className="starting-screen-section">
-      <h1>Poke Memo Game</h1>
       <dialog open id="modal">
         <div className="modal-content">
           <button onClick={handleClick}>Easy</button>
